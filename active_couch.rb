609d72889a8ffb6272ba4123d0e80a3dba2ae21f
Kevin Jing Qiu <kevin@idempotent.ca>
2016-11-08 13:11:05 -0500

    Make the urls configurable

--- active_couch.rb
+++ active_couch.rb
@@ -1,3 +1,4 @@
+require 'optparse'
 require 'httparty'
 require 'json'
 require 'date'
@@ -40,25 +41,37 @@ module ActiveCouch
   end
 end
 
-tasks = ActiveCouch::get_tasks ['http://admin:password@localhost:15984', 'http://admin:password@localhost:25984']
+def main(couchdb_urls)
+  tasks = ActiveCouch::get_tasks couchdb_urls
 
-def render_eta(eta)
-  absolute_time = DateTime.strptime(eta[:time].to_s, "%s")
-  relative_hours = (eta[:relative] / 3600).floor
-  relative_minutes = ((eta[:relative] - (relative_hours * 3600)) / 60).floor
-  "#{absolute_time} (in #{relative_hours} hours #{relative_minutes} minutes)"
+  def render_eta(eta)
+    absolute_time = DateTime.strptime(eta[:time].to_s, "%s")
+    relative_hours = (eta[:relative] / 3600).floor
+    relative_minutes = ((eta[:relative] - (relative_hours * 3600)) / 60).floor
+    "#{absolute_time} (in #{relative_hours} hours #{relative_minutes} minutes)"
+  end
+
+  def render_progress_bar(done, total, bar_length=40)
+    completed = ((1.0 * done / total) * bar_length).floor
+    remaining = bar_length - completed
+    "[#{"#" * completed}#{"_" * remaining}] (#{(100.0*done/total).round(2)}%)"
+  end
+
+  tasks.each { |task|
+    puts "#{task.name}"
+    puts "  progress: #{render_progress_bar(task.changes_done, task.total_changes)}"
+    puts "  rate: #{task.rate} cps (changes-per-second)"
+    puts "  eta: #{render_eta(task.eta)}"
+    puts
+  }
+
+  return 0
 end
 
-def render_progress_bar(done, total, bar_length=40)
-  completed = ((1.0 * done / total) * bar_length).floor
-  remaining = bar_length - completed
-  "[#{"#" * completed}#{"_" * remaining}] (#{(100.0*done/total).round(2)}%)"
+if ARGV.length == 0
+  puts 'Usage: active_couch.rb <couchdb_urls...>'
+  puts 'Example: ruby active_couch.rb http://admin:password@localhost:5984 http://admin:password@yourdomain.com:5984'
+  exit 1
 end
 
-tasks.each { |task|
-  puts "#{task.name}"
-  puts "  progress: #{render_progress_bar(task.changes_done, task.total_changes)}"
-  puts "  rate: #{task.rate} cps (changes-per-second)"
-  puts "  eta: #{render_eta(task.eta)}"
-  puts
-}
+exit main(ARGV)
