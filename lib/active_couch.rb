a05c05ecf033df538869b45515129adb6cb5b5ac
Kevin Jing Qiu <kevin@idempotent.ca>
2016-11-16 11:44:31 -0500

    Update render

--- lib/active_couch.rb
+++ lib/active_couch.rb
@@ -18,23 +18,39 @@ module ActiveCouch
     def render
       "#{name}"
     end
+
+    def render_progress_bar(done, total, bar_length=40)
+      completed = ((1.0 * done / total) * bar_length).floor
+      remaining = bar_length - completed
+      "[#{"#" * completed}#{"." * remaining}] (#{(100.0*done/total).round(2)}%)"
+    end
   end
 
   class ReplicationTask < Task
     def name
-      "Replication : #{@couchdb_url} - #{replication_id}"
+      "Replication: #{@couchdb_url}/id:#{replication_id}, target:#{target}"
+    end
+
+    def render
+      ["#{name}",
+       "#{render_progress_bar(progress, 100)}"].join("\t")
     end
   end
 
   class CompactionTask < Task
     def name
-      "Compaction: #{@couchdb_url}"
+      "Compaction: #{@couchdb_url}/#{database}:#{design_document}"
+    end
+
+    def render
+      ["#{name}",
+       "#{render_progress_bar(progress, 100)}"].join("\t")
     end
   end
 
   class IndexingTask < Task
     def name
-      "Indexing: #{@couchdb_url} - #{database}:#{design_document}"
+      "Indexing: #{@couchdb_url}/#{database}:#{design_document}"
     end
 
     def rate
@@ -55,23 +71,16 @@ module ActiveCouch
       "#{absolute_time} (in #{relative_hours} hours #{relative_minutes} minutes)"
     end
 
-    def render_progress_bar(done, total, bar_length=40)
-      completed = ((1.0 * done / total) * bar_length).floor
-      remaining = bar_length - completed
-      "[#{"#" * completed}#{"_" * remaining}] (#{(100.0*done/total).round(2)}%)"
-    end
-
     def render
       ["#{name}",
-       "  progress: #{render_progress_bar(changes_done, total_changes)}",
-       "  rate: #{rate} cps (changes-per-second)",
-       "  eta: #{render_eta(eta)}",
-      ].join("\n")
+       "#{render_progress_bar(changes_done, total_changes)}",
+       "#{rate} cps (changes-per-second)",
+       "eta: #{render_eta(eta)}",
+      ].join("\t")
     end
   end
 
   def self.create_task(couchdb_url, task)
-    puts task
     if task['type'] == 'replication'
       ReplicationTask.new(couchdb_url, task)
     elsif task['type'] == 'view_compaction'
